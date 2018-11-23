b488fe115ca840302e287a3d14db5913b6e223bd
Maria <murikmarusia@gmail.com>
2018-11-23 11:19:07 +0200

    formated code

--- src/js/main.js
+++ src/js/main.js
@@ -6,31 +6,30 @@
 
   const items = document.querySelectorAll('.custom-select option');
 
-    const dropdownContent = document.createElement('div');
-    dropdownContent.classList.add('select-items');
-    dropdownContent.classList.add('select-hide');
-    for (let i = 0; i < items.length; i++) {
-      const div = document.createElement('div');
-      dropdownContent.appendChild(div);
-      div.innerHTML = items[i].textContent;
-      document.querySelector('.select-selected').appendChild(dropdownContent);
-    }
+  const dropdownContent = document.createElement('div');
+  dropdownContent.classList.add('select-items');
+  dropdownContent.classList.add('select-hide');
+  for (let i = 0; i < items.length; i++) {
+    const div = document.createElement('div');
+    dropdownContent.appendChild(div);
+    div.innerHTML = items[i].textContent;
+    document.querySelector('.select-selected').appendChild(dropdownContent);
+  }
 
   dropdownBtn.addEventListener('click', () => {
-    document.querySelector('.select-selected').classList.toggle('select-arrow-active');
+    document
+      .querySelector('.select-selected')
+      .classList.toggle('select-arrow-active');
     document.querySelector('.select-items').classList.toggle('select-hide');
   });
-
 
   const itemsBtn = document.querySelectorAll('.select-items div');
   itemsBtn.forEach((element) => {
     element.addEventListener('click', (e) => {
       e.preventDefault();
       const activeName = element.textContent;
-      
+
       // dropdownBtn.innerHTML = element.textContent;
-    
     });
   });
-
 }());
